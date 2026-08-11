6a0b4776c9dea9f4744d30086b5a3cc1dfb695db
N Giles <139185628+ngiles1@users.noreply.github.com>
2026-08-11 21:01:22 +0100

    Add locations row to Contact page, reusing TriItem and LinkButton

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,51 @@
-import Image from "next/image";
-import Link from "next/link";
-// import styles from "./page.module.css";
+import clsx from "clsx";
+import TriItem from "@/app/_components/tri-item";
+import LinkButton from "@/app/_components/link-button";
 
 export default function Contact() {
   return (
-    <div>contact page</div>
+    <div>
+      <section
+        className={clsx(
+          "flex",
+          "flex-col",
+          "gap-8",
+          "lg:grid",
+          "lg:grid-cols-3",
+          "max-sm:px-2",
+          "mb-18",
+        )}
+      >
+        <TriItem
+          bgRot={0}
+          image="/assets/shared/desktop/illustration-canada.svg"
+          title="Canada"
+        >
+          <LinkButton href="/locations" variant="peach">
+            See location
+          </LinkButton>
+        </TriItem>
+
+        <TriItem
+          bgRot={270}
+          image="/assets/shared/desktop/illustration-australia.svg"
+          title="Australia"
+        >
+          <LinkButton href="/locations" variant="peach">
+            See location
+          </LinkButton>
+        </TriItem>
+
+        <TriItem
+          bgRot={90}
+          image="/assets/shared/desktop/illustration-united-kingdom.svg"
+          title="United Kingdom"
+        >
+          <LinkButton href="/locations" variant="peach">
+            See location
+          </LinkButton>
+        </TriItem>
+      </section>
+    </div>
   )
 };
